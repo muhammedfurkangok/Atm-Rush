1d216c39739a2735d2c3048a4b437671cf4ccf54
gkkangk@gmail.com <gkkangk@gmail.com>
2024-03-13 14:12:44 +0300

    Minigame System & Save System Finalized

---
+++ Assets/_Modules/SaveModule/Scripts/Commands/LoadCommand.cs
@@ -0,0 +1,13 @@
+using _Modules.SaveModule.Scripts.Interfaces;
+
+namespace _Modules.SaveModule.Scripts.Commands
+{
+    public class LoadCommand
+    {
+        public T Execute<T>(string dataKey,string path) where T: ISaveableEntity
+        {
+            T objectReturn = ES3.Load<T>(dataKey, path);
+            return objectReturn;
+        }
+    }
+}
